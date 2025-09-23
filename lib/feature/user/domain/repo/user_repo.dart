ed4702e226b727f9c5cd5066c3bca27f3e66f5e1
A-Helal <adbelrahman21-00303@student.eelu.edu.eg>
2025-09-23 15:13:48 +0300

    -Add user params

--- lib/feature/user/domain/repo/user_repo.dart
+++ lib/feature/user/domain/repo/user_repo.dart
@@ -1,7 +1,8 @@
 import 'package:clean_arch_prac/core/errors/failure.dart';
+import 'package:clean_arch_prac/core/params/user_params.dart';
 import 'package:clean_arch_prac/feature/user/domain/entites/user_entity.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class UserRepo{
-  Future<Either<Failure,UserEntity>>getUser();
+abstract class UserRepo {
+  Future<Either<Failure, UserEntity>> getUser({required UserParams params});
 }
--- lib/feature/user/domain/usecases/get_user.dart
+++ lib/feature/user/domain/usecases/get_user.dart
@@ -1,4 +1,5 @@
 import 'package:clean_arch_prac/core/errors/failure.dart';
+import 'package:clean_arch_prac/core/params/user_params.dart';
 import 'package:clean_arch_prac/feature/user/domain/entites/user_entity.dart';
 import 'package:clean_arch_prac/feature/user/domain/repo/user_repo.dart';
 import 'package:dartz/dartz.dart';
@@ -8,7 +9,7 @@ class GetUser {
 
   GetUser({required this.userRepo});
 
-  Future<Either<Failure, UserEntity>> call() {
-    return userRepo.getUser();
+  Future<Either<Failure, UserEntity>> call({required UserParams params}) {
+    return userRepo.getUser(params: params);
   }
 }
