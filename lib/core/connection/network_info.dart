688c00565d6613537d91d1bf622f5572d40dec77
A-Helal <adbelrahman21-00303@student.eelu.edu.eg>
2025-10-01 04:25:13 +0300

    - finish the entire app with lottie and some edits

--- lib/core/connection/network_info.dart
+++ lib/core/connection/network_info.dart
@@ -2,6 +2,8 @@ import 'package:data_connection_checker_tv/data_connection_checker.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+
+  Stream<bool> get onConnectionChange;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
@@ -18,4 +20,9 @@ class NetworkInfoImpl implements NetworkInfo {
 
   @override
   Future<bool> get isConnected => connectionChecker.hasConnection;
+
+  @override
+  Stream<bool> get onConnectionChange => connectionChecker.onStatusChange.map(
+    (status) => status == DataConnectionStatus.connected,
+  );
 }
--- lib/feature/user/presentation/screens/user_screen.dart
+++ lib/feature/user/presentation/screens/user_screen.dart
@@ -1,35 +1,77 @@
+import 'package:clean_arch_prac/core/connection/network_info.dart';
 import 'package:clean_arch_prac/feature/user/presentation/cubit/user_cubit.dart';
 import 'package:clean_arch_prac/feature/user/presentation/widgets/custom_appbar.dart';
 import 'package:clean_arch_prac/feature/user/presentation/widgets/custom_slider_bar.dart';
-import 'package:clean_arch_prac/feature/user/presentation/widgets/get_user_button.dart';
 import 'package:clean_arch_prac/feature/user/presentation/widgets/user_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final networkInfo = NetworkInfoImpl();
     return BlocProvider(
       create: (context) => UserCubit()..eitherFailureOrUser(1),
       child: Scaffold(
         appBar: const CustomAppbar(),
-        body: BlocConsumer<UserCubit, UserState>(
-          listener: (context, state) {},
-          builder: (context, state) {
-            if (state is GetUserSuccessfully) {
-              return ListView(
-                children: [
-                  UserData(user: state.user),
-                  const CustomSliderBar(),
-
-                ],
+        body: StreamBuilder(
+          initialData: false,
+          stream: networkInfo.onConnectionChange,
+          builder: (context, snapshots) {
+            final connected = snapshots.data ?? false;
+            if (!connected) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Spacer(),
+                    Text(
+                      "Ooops!",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'No Internet Connection',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffFFA61A),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    Lottie.asset("assets/no_connection.json"),
+                    SizedBox(height: 50),
+                  ],
+                ),
               );
-            } else if (state is GetUserFailure) {
-              return Center(child: Text(state.errMessage));
             } else {
-              return const Center(child: CircularProgressIndicator());
+              return BlocConsumer<UserCubit, UserState>(
+                listener: (context, state) {},
+                builder: (context, state) {
+                  if (state is GetUserSuccessfully) {
+                    return ListView(
+                      children: [
+                        UserData(user: state.user),
+                        const CustomSliderBar(),
+                      ],
+                    );
+                  } else if (state is GetUserFailure) {
+                    return Center(child: Text(state.errMessage));
+                  } else {
+                    return Column(
+                      children: [Lottie.asset("assets/loading.json")],
+                    );
+                  }
+                },
+              );
             }
           },
         ),
